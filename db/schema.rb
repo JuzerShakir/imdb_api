feffa1f57f45e7da0129f90398e69e55523c9499
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-13 20:59:06 +0530

    drop director attribute from entertainments table as new table relation has been setup for it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_123149) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_152512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_123149) do
 
   create_table "entertainments", force: :cascade do |t|
     t.string "title"
-    t.string "director"
     t.float "ratings"
     t.string "tagline"
     t.string "story"
