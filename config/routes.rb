a23a8cc869cce646a2eac0e28476082ccfb4e7f1
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-16 11:32:50 +0530

    update routes to only create and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   namespace :api do
-    resource :entertainment
+    resource :entertainment, only: [:create, :update]
   end
 end
