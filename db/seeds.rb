dc367b789d5c9b7470a187e3660580033df773fe
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-16 11:50:35 +0530

    New constant for group, update variable names

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,23 @@ if Entertainment.exists?
 end
 
 N = 1
+G = 30
 
 files = %w(movie_links.txt tv-series_links.txt)
 
 files.each do | file |
     file_content = File.open("#{Dir.pwd}/lib/seed_data/#{file}")
-    all_shows = file_content.readlines.map(&:chomp)
-    limited_shows = all_shows.first(N)
-    show_batches = limited_shows.in_groups_of(30, false)
-    total_batches = show_batches.count
+    all_urls = file_content.readlines.map(&:chomp)
+    limited_urls = all_urls.first(N)
+    groups_of_urls = limited_urls.in_groups_of(G, false)
+    number_of_groups = groups_of_urls.count
 
-    show_batches.each_with_index do | show_batch, i |
-        show_batch.each do | show_url |
-            connect_n_fetch(show_url)
+    groups_of_urls.each_with_index do | group_of_urls, i |
+        group_of_urls.each do | url |
+            connect_n_fetch(url)
             set_show_values
             @browser.close
         end
-        sleep(30) if show_batch.count == 30 && i+1 != total_batches
+        sleep(30) if group_of_urls.count == G && i+1 != number_of_groups
     end
 end
