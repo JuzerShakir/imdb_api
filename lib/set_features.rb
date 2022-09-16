1f2b41a77baeb021fceeddb1729dd748213de74e
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-16 11:31:13 +0530

    Update instance variable names

--- lib/set_features.rb
+++ lib/set_features.rb
@@ -1,23 +1,23 @@
 module SetFeatures
-    def set_feature_values
-        @features = %i(title identifier ratings runtime release_date revenue budget tagline popularity)
+    def set_attribute_values
+        @all_show_attributes = %i(title identifier ratings runtime release_date revenue budget tagline popularity)
 
-        @feature_values = [get_title, get_identifier, get_ratings, get_runtime, get_release_date, get_revenue,
+        @all_show_values = [get_title, get_identifier, get_ratings, get_runtime, get_release_date, get_revenue,
             get_budget, get_tagline, get_popularity]
     end
 
     def set_relational_values
-        @relational_features = [get_genres, get_stars, get_producers, get_directors]
+        @relational_data = [get_genres, get_stars, get_producers, get_directors]
     end
 
     def set_show_values
         @show = content_type == "TV" ? TvShow.new : Movie.new
         @show.url = @url
 
-        set_feature_values
-        @show.update( @features.zip(@feature_values).to_h )
+        set_attribute_values
+        @show.update( @all_show_attributes.zip(@all_show_values).to_h )
 
         set_relational_values if @show.persisted?
-        @show.method("set_relations").call(@relational_features)
+        @show.method("set_relations").call(@relational_data)
     end
 end
--- lib/update_features.rb
+++ lib/update_features.rb
@@ -1,11 +1,13 @@
 module UpdateFeatures
     def set_update_values
-        @features = %i(ratings popularity revenue budget)
-        @feature_values = [get_ratings, get_popularity, get_revenue, get_budget]
+        @dynamic_show_attributes = %i(ratings popularity revenue budget)
+        @dynamic_show_values = [get_ratings, get_popularity, get_revenue, get_budget]
     end
 
     def update_show_values
         set_update_values
-        @show.update( @features.zip(@feature_values ).to_h )
+        @show.update(
+            @dynamic_show_attributes.zip(@dynamic_show_values).to_h
+        )
     end
 end
