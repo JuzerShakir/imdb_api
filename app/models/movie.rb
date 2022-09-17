4d981dbec69be39f697b146e8542f0d19e6097b2
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-17 18:10:19 +0530

    set up callback to calculate profit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < Entertainment
+    # * callbacks
+    before_save :set_profit
+
+    private
+        def set_profit
+            self.profit = self.revenue - self.budget
+        end
 end
