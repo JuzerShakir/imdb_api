9ea4f263d60010c9a3194382cebaebd942021528
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-17 18:24:15 +0530

    give commenst for Constants and change a constants name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ if Entertainment.exists?
     models.each { |model| model.constantize.destroy_all }
 end
 
-N = 1
-G = 30
+N = 1                  # * Values acceptable between 1 to 250
+URLS_IN_A_GROUP = 30      # ! Do NOT set value greater than 30 to avoid robot confiramtion
 
 files = %w(movie_links.txt tv-series_links.txt)
 
@@ -14,7 +14,7 @@ files.each do | file |
     file_content = File.open("#{Dir.pwd}/lib/seed_data/#{file}")
     all_urls = file_content.readlines.map(&:chomp)
     limited_urls = all_urls.first(N)
-    groups_of_urls = limited_urls.in_groups_of(G, false)
+    groups_of_urls = limited_urls.in_groups_of(URLS_IN_A_GROUP, false)
     number_of_groups = groups_of_urls.count
 
     groups_of_urls.each_with_index do | group_of_urls, i |
@@ -23,6 +23,6 @@ files.each do | file |
             set_show_values
             @browser.close
         end
-        sleep(30) if group_of_urls.count == G && i+1 != number_of_groups
+        sleep(30) if group_of_urls.count == URLS_IN_A_GROUP && i+1 != number_of_groups
     end
 end
