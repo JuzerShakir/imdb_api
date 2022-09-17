f593bea32e21e1363aa984aeb4e5af64bd929042
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-17 18:09:23 +0530

    add comment for validations and associations

--- app/models/entertainment.rb
+++ app/models/entertainment.rb
@@ -1,9 +1,11 @@
 class Entertainment < ApplicationRecord
+    # * Associations
     has_and_belongs_to_many :genres
     has_and_belongs_to_many :stars
     has_and_belongs_to_many :producers
     has_and_belongs_to_many :directors
 
+    # * validations
     validates_presence_of :identifier
     validates_uniqueness_of :identifier, message: "must be unique"
 
