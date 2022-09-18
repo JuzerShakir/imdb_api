173f2e534feebb692641e79b45a2b2f576a45fad
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-09-18 12:52:58 +0530

    browser will close after dynamic features are update for update action

--- app/imdb/update_features.rb
+++ app/imdb/update_features.rb
@@ -4,11 +4,14 @@ module UpdateFeatures
     def set_update_values
         @dynamic_show_attributes = %i(ratings popularity revenue budget)
         @dynamic_show_values = [get_ratings, get_popularity, get_revenue, get_budget]
+        if @show.type = "TvShow"
+            @dynamic_show_attributes << :runtime
+            @dynamic_show_values << get_runtime
+        end
     end
 
     def update_show_values
         set_update_values
-        @browser.close
         @show.update(
             @dynamic_show_attributes.zip(@dynamic_show_values).to_h
         )
